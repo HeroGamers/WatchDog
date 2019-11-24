ce5383b8999825b89854a6c62dfde5e3c2f5b632
Fido2603 <hero@herogamers.xyz>
2019-11-24 05:35:54 +0100

    Botinfo fix

--- cogs/info.py
+++ cogs/info.py
@@ -42,9 +42,10 @@ class Info(commands.Cog):
                                 value="%s" % discord.utils.snowflake_time(ctx.bot.user.id).strftime(
                                     "%Y-%m-%d %H:%M:%S"), inline=True)
                 embed.add_field(name="Guilds", value="%s" % len(bot.guilds), inline=True)
+                ban_list_guild = bot.get_guild(int(os.getenv('banlistguild')))
                 ban_list = await ctx.guild.bans()
                 embed.add_field(name="Global Bans", value="%s" % len(ban_list), inline=True)
-                embed.add_field(name="Central Server", value=bot.get_guild(int(os.getenv('banlistguild'))).name,
+                embed.add_field(name="Central Server", value=ban_list_guild.name,
                                 inline=True)
                 # embed.add_field(name="Log channel", value="<#%s>" % bot.get_channel(int(os.getenv('botlog'))).id, inline=True)
                 embed.set_footer(text="%s - Global WatchDog Moderator" % ctx.author.name,
