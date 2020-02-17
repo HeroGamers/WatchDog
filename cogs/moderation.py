1728e05389fbce4ddabbfe76268d2c5c90060096
Fido2603 <hero@herogamers.xyz>
2020-02-17 22:53:18 +0100

    Fixed # not showing in the database on DiscordTags

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -61,7 +61,7 @@ class Moderation(commands.Cog):
             if unban:
                 database.invalidateBan(user.id)
             else:
-                database.newBan(userid=user.id, discordtag=user.name + user.discriminator, moderator=ctx.author.id,
+                database.newBan(userid=user.id, discordtag=user.name + "#" + user.discriminator, moderator=ctx.author.id,
                                 reason=reason, guild=ctx.guild.id, avatarurl=user.avatar_url)
 
         # Function to ban
