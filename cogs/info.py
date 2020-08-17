a13538e0e7081a096671bd56274258ab47ecc020
Hero <15278940+HeroGamers@users.noreply.github.com>
2020-08-17 23:14:48 +0200

    temporary DM for privacy policy

--- cogs/info.py
+++ cogs/info.py
@@ -50,6 +50,7 @@ class Info(commands.Cog):
         embed.add_field(name="Global Bans", value="%s" % len(ban_list), inline=True)
         embed.add_field(name="Central Server", value=ban_list_guild.name,
                         inline=True)
+        embed.add_field(name="Privacy Policy", value="For the Privacy Policy, please send a DM to HeroGamers through the support server!", inline=True)
         embed.set_footer(text="%s" % ctx.author.name,
                          icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
